337941477bc370e2f90fa96b2159c01ef160bb13
yeona <aya813@naver.com>
2024-07-28 23:08:08 +0900

    feat: 명언 찜 버튼 추가

--- src/pages/resultQuote/ResultQuotePage.tsx
+++ src/pages/resultQuote/ResultQuotePage.tsx
@@ -2,20 +2,33 @@ import Button from 'components/common/Button';
 import Comment from 'components/resultQuote/Comment';
 import ResultQuote from 'components/resultQuote/ResultQuote';
 import WriteComment from 'components/resultQuote/WriteComment';
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+// @TODO 해당 명언 댓글 조회
+
 const ResultQuotePage = () => {
+  const [isLike, setIsLike] = useState(false);
   const navigate = useNavigate();
 
   const handleClick = () => {
+    // @TODO like 관련 post 요청 보내야함
     navigate('/quoteList');
+  };
+
+  const handleLike = () => {
+    setIsLike(!isLike);
   };
 
   return (
     <div className="flex flex-col items-center pt-[30px] gap-[30px]">
       <div className="flex flex-col gap-[20px] w-[300px] rounded-xl bg-white shadow-custom p-5">
-        <ResultQuote />
+        <ResultQuote
+          imageUrl="/images/quoteImage1.png"
+          quote="  오늘의 명언은 이거야 ~~~오늘의 명언은 아주아주 길다 길다기달다길다"
+          isLike={isLike}
+          handleLike={handleLike}
+        />
         <WriteComment />
         <Comment
           profileImage="/images/quoteImage2.jpg"
