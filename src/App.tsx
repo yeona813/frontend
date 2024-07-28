ec101ebd8e5af7cb23d5bba439c7e77bc736ad41
yeona <aya813@naver.com>
2024-07-29 01:23:51 +0900

    feat: footer 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import Footer from 'components/common/Footer';
 import Header from 'components/common/Header';
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Router from 'Router';
 
 function App() {
+  const location = useLocation();
+
   return (
-    <div className="w-[376px] h-screen mx-auto bg-yellow-FF">
+    <div className="relative w-[376px] h-[812px] mx-auto bg-yellow-FF overflow-auto scrollbar-hide pb-[80px]">
       <Header />
       <Router />
+      {location.pathname !== '/login' && location.pathname !== '/register' && (
+        <Footer />
+      )}
     </div>
   );
 }
