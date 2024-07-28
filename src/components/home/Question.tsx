b270b2ad50a6e420af88ce180f896feaffa43b9e
yeona <aya813@naver.com>
2024-07-28 22:25:42 +0900

    feat: textarea 값 관리 추가

--- src/components/home/Question.tsx
+++ src/components/home/Question.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
-const Question = () => {
+interface QuestionProps {
+  text: string;
+  handleTextChange: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+const Question = ({ text, handleTextChange }: QuestionProps) => {
   return (
     <div className="flex flex-col items-center w-[300px] h-[500px] rounded-xl bg-white shadow-custom gap-[20px]">
       <p className="font-semibold mt-[30px]">오늘은 무슨 고민이 있나요?</p>
@@ -8,6 +13,8 @@ const Question = () => {
       <textarea
         className="w-[240px] h-[350px] bg-white resize-none outline-none scrollbar-hide leading-8 text-sm"
         placeholder="고민이나 필요한 조언에 대해 자세히 적어주세요. 구체적인 글이 더 정확한 명언을 찾는 데 도움이 됩니다."
+        value={text}
+        onChange={handleTextChange}
       />
     </div>
   );
