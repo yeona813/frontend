33afb833d15a6e1a25a16eed8d1c6f048eed8f61
yeona <aya813@naver.com>
2024-07-28 23:19:39 +0900

    feat: 댓글 state 구현

--- src/components/resultQuote/WriteComment.tsx
+++ src/components/resultQuote/WriteComment.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 const WriteComment = () => {
+  const [comment, setComment] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(event.target.value);
+  };
+
+  const handleClick = () => {
+    console.log(comment);
+    // 댓글 post 요청
+  };
+
   return (
-    <div className="relative flex flex-col gap-[10px]">
+    <div className="flex flex-col gap-[10px]">
       <span className="text-sm font-semibold">댓글</span>
-      <textarea
-        className="relative border border-gray-200 p-3 rounded-xl h-[100px] resize-none outline-none scrollbar-hide text-xs"
-        placeholder="댓글 작성하기"
-      />
-      <button
-        type="button"
-        className="absolute right-3 bottom-3 border border-gray-200 rounded-xl w-[60px] h-[30px] text-xs"
-      >
-        입력
-      </button>
+      <div className="relative w-[260px] h-[110px] border border-gray-200 rounded-xl p-3">
+        <textarea
+          className="w-[236px] h-[50px] resize-none outline-none scrollbar-hide text-xs"
+          placeholder="댓글 작성하기"
+          value={comment}
+          onChange={handleChange}
+        />
+        <button
+          type="button"
+          className="absolute right-3 bottom-3 border border-gray-200 rounded-xl w-[60px] h-[30px] text-xs"
+          onClick={handleClick}
+        >
+          입력
+        </button>
+      </div>
     </div>
   );
 };
