81cf5341b0d28f130bdb0c5a89d653c912b06941
yeona <aya813@naver.com>
2024-08-02 22:04:04 +0900

    feat: w-screen 속성 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -11,23 +11,19 @@ const Header = () => {
   };
 
   return (
-    <div className="fixed top-0 w-[376px] z-20 bg-yellow-FF grid grid-cols-5 items-center py-5 h-[70px] shadow-sm">
-      <div className="text-center">
-        <button onClick={onClickMenu} type="button">
-          <img
-            className="w-[25px] h-[25px] cursor-pointer"
-            src="/icons/menu.png"
-            alt="메뉴"
-          />
-        </button>
-      </div>
-
-      <Link to="/" className="text-[25px] col-span-3 text-center">
+    <div className="fixed top-0 w-screen z-20 bg-yellow-FF flex items-center justify-center py-5 h-[70px] shadow-sm">
+      <button onClick={onClickMenu} type="button">
+        <img
+          className="absolute top-5 left-[30px] w-[25px] h-[25px] cursor-pointer"
+          src="/icons/menu.png"
+          alt="메뉴"
+        />
+      </button>
+      <Link to="/" className="text-[25px]">
         로고
       </Link>
-
       <div
-        className={`fixed top-0 flex bg-white w-[250px] h-[812px] z-40 ${menuOpened ? 'transform-translatex-show' : 'transform-translatex-hide'} `}
+        className={`fixed top-0 left-0 flex bg-white w-[250px] h-screen z-40 ${menuOpened ? 'transform-translatex-show' : 'transform-translatex-hide'} `}
       >
         <SideMenu />
       </div>
