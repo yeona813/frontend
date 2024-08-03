8f7029cfe7bb9ebb167889d4a06dc46730b8c24d
yeona <aya813@naver.com>
2024-08-03 19:08:05 +0900

    feat: background 이미지 설정

--- src/components/home/Question.tsx
+++ src/components/home/Question.tsx
@@ -10,6 +10,7 @@ const Question = ({ text, handleTextChange }: QuestionProps) => {
     <div className="flex flex-col items-center w-[300px] h-[350px] rounded-xl bg-white shadow-custom gap-[20px]">
       <p className="font-semibold mt-[30px]">오늘은 무슨 고민이 있나요?</p>
       <div className="bg-yellow-FF w-[250px] h-[2px] mb-5" />
+      {/* yellow-ff 말고 다른걸로 해야함 */}
       <textarea
         className="w-[240px] h-[350px] bg-white resize-none outline-none scrollbar-hide leading-8 text-sm"
         placeholder="고민이나 필요한 조언에 대해 자세히 적어주세요. 구체적인 글이 더 정확한 명언을 찾는 데 도움이 됩니다."
