cd9b86fcfe8138f1d59f368df3ff7d9238f04e1a
yeona <aya813@naver.com>
2024-08-07 00:54:21 +0900

    feat: 리다이렉션 수정

--- src/components/login/LoginButton.tsx
+++ src/components/login/LoginButton.tsx
@@ -30,7 +30,7 @@ const LoginButton = ({
 
   const onClickKakaoLogin = () => {
     const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
-    const REDIRECT_URI = 'http://localhost:3000/auth/callback/'; // 프론트엔드의 Redirect URI
+    const REDIRECT_URI = 'https://hackathonpebble.vercel.app/auth/callback/'; // 프론트엔드의 Redirect URI
     console.log(REST_API_KEY, REDIRECT_URI);
     const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
--- src/pages/login/Redirection.tsx
+++ src/pages/login/Redirection.tsx
@@ -11,7 +11,7 @@ const Redirection = () => {
       try {
         const res = await axios({
           method: 'get',
-          url: `http://15.164.27.255/accounts/kakao/callback/?code=${code}`,
+          url: `https://pebblequote.site/accounts/kakao/callback/?code=${code}`,
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8', // json형태로 데이터를 보내겠다는 뜻
           },
